12a79fe242a899a5b0cf3dfc149c4c21841879ee
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-08-11 21:37:21 -0400

    Add utility functions related to files and directories

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
 lib: let
   inherit (lib) filterAttrs;
-  inherit (builtins) attrNames filter pathExists concatMap readDir;
+  inherit (builtins) attrNames filter pathExists concatMap readDir match;
 in rec {
-  getPaths = file: root:
-    filter pathExists (
-      map (dir: root + "/${dir}/${file}") (
-        attrNames (filterAttrs (name: type: type == "directory") (readDir root))
-      )
-    );
+  getDirs = root: map (dir: root + "/${dir}") (attrNames (filterAttrs (name: type: type == "directory") (readDir root)));
+  
+  getFiles = root: map (file: root + "/${file}") (attrNames (filterAttrs (name: type: type == "regular") (readDir root)));
+  
+  getNix = root: filter (file: match ''*\.nix'' file) (getFiles root); 
+  
+  getPaths = file: root: filter pathExists (map (dir: dir + "/${file}") (getDirs root));
+  
   getModules = concatMap (getPaths "default.nix");
 }
-
-

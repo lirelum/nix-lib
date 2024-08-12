9a8c5de6fb28e5d3f98e0f073a5282e11ee92e70
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-08-12 00:45:09 -0400

    Remove unnecessary output

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Simple helper functions for my system";
 
   outputs = {nixpkgs, ...}: {
-    lib = import ./default.nix nixpkgs.lib;
     formatter = {
       "x86_64-linux" = nixpkgs.legacyPackages."x86_64-linux".alejandra;
       "aarch64-darwin" = nixpkgs.legacyPackages."aarch64-darwin".alejandra;

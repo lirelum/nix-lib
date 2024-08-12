e3e046df768b67bd4fd19dd53e62ebdda8d93f7c
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-08-12 01:10:06 -0400

    Fix bug in getNix function

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 lib: let
-  inherit (lib) filterAttrs;
-  inherit (builtins) attrNames filter pathExists concatMap readDir match;
+  inherit (lib) filterAttrs hasSuffix;
+  inherit (builtins) attrNames filter pathExists concatMap readDir;
 in rec {
   getDirs = root: map (dir: root + "/${dir}") (attrNames (filterAttrs (name: type: type == "directory") (readDir root)));
 
   getFiles = root: map (file: root + "/${file}") (attrNames (filterAttrs (name: type: type == "regular") (readDir root)));
 
-  getNix = root: filter (file: match ''.*\.nix'' file) (getFiles root);
+  getNix = root: map (file: root + "/${file}") (attrNames (filterAttrs (name: type: hasSuffix ".nix" name) (readDir root)));
 
   getPaths = file: root: filter pathExists (map (dir: dir + "/${file}") (getDirs root));
 
